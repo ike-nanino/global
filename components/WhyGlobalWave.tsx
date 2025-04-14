230e04d7a6070306bc8c7ddf9b8fab17ad356263
ike-nanino <ikesemanu@gmail.com>
2025-04-14 15:40:50 -0400

    Update spacing for WhyGlobalWave component; adjust margin for better layout and fix heading size

--- components/WhyGlobalWave.tsx
+++ components/WhyGlobalWave.tsx
@@ -7,10 +7,10 @@ function WhyGlobalWave() {
         >
 
 
-            <div className='lg:mt-38 lg:pl-26'>
+            <div className='mt-10 lg:mt-38 lg:pl-26'>
 
 
-                <h1 className='text-2xl lg:text-4xl  text-left mb-4 font-bold  text-white '>Whatever Your Freight Forwarding And Customs Clearance Needs, You’ve Come To The Right Place!</h1> 
+                <h1 className='text-xl lg:text-4xl  text-left mb-4 font-bold  text-white '>Whatever Your Freight Forwarding And Customs Clearance Needs, You’ve Come To The Right Place!</h1> 
 
                 <h4 className='text-2xl text-left mb-4 font-medium text-white '>
 An opportunity to accelerate growth with a streamlined fulfilment solution. Creating powerful partnerships with our clients and helping build lifelong relationships.</h4>
